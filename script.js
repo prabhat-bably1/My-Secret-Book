be90c65f68873bfcf9bb3354423dd24186370d85
prabhat-bably1 <prabhatr.seth04@gmail.com>
2026-03-22 23:29:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function flipBook() {
-    document.getElementById("bookBox").classList.toggle("flip");
+function nextPage() {
+    document.getElementById("page2").innerHTML = `
+        <h3>Welcome to your Secret World ✨</h3>
+        <p>
+        Start your journey here... <br>
+        Share your thoughts, jokes, shayari and feelings 💛
+        </p>
+    `;
 }
