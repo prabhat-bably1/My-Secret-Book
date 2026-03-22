2ef693f83f5a45cd30e21b31168214853530f12b
prabhat-bably1 <prabhatr.seth04@gmail.com>
2026-03-22 12:20:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let currentPage = 0;
+const pages = document.querySelectorAll(".page");
+
+function nextPage() {
+    if (currentPage < pages.length) {
+        pages[currentPage].style.zIndex = currentPage;
+        pages[currentPage].classList.add("flipped");
+        currentPage++;
+    }
+}
+
+function prevPage() {
+    if (currentPage > 0) {
+        currentPage--;
+        pages[currentPage].classList.remove("flipped");
+    }
+}
+
+/* Popup */
 function showLogin() {
     document.getElementById("loginBox").style.display = "block";
 }
