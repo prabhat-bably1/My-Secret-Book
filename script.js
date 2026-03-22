9fa3ea8364d1dd9f25abaf684950ec7863085861
prabhat-bably1 <prabhatr.seth04@gmail.com>
2026-03-22 18:24:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function goHome() {
-    alert("Home clicked");
-}
-
-function goAbout() {
-    alert("About clicked");
-}
-
-function goLogin() {
-    alert("Login clicked");
-}
+console.log("Website Loaded");
